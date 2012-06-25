b7d2efc91df54f84cc1816d6edd570bc82af7623
Amin Oruji <aminpy@gmail.com>
2012-06-25 11:03:01 +0430

    model and field created

---
+++ jalali_convertor.py
@@ -0,0 +1,21 @@
+from osv import osv, fields
+
+
+class JalaliConvertor(osv.osv_memory):
+    _name = 'jalali.convertor'
+    _inherit = 'ir.wizard.screen'
+
+    _columns = {
+        'ir_module_module_id': fields.many2one('ir.module.module',
+                                               'IR Module Module'),
+        'state': fields.selection([('init', 'init'), ('done', 'done')],
+                                  'state', readonly=True),
+    }
+
+    _defaults = {'state': 'init'}
+
+    def do_convert(self, cr, uid, ids, context=None):
+        print "durud"
+        pass
+
+JalaliConvertor()
