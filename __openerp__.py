cdfd1b45ccb940f3a3688ed12d966bda67e4ff38
Amin Oruji <aminpy@gmail.com>
2012-06-25 10:55:36 +0430

    project files added

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Jalali Convertor',
+    'version': '1.0',
+    'category': 'Tools',
+    'author': 'Amin Oruji',
+    'depends': ['base'],
+    'description': "Jalali Convertor",
+    'update_xml': ['jalali_convertor_view.xml'],
+    'installable': True,
+    'active': False,
+}
