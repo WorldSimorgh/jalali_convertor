006f68f2f00cbb1066298abd830f75de51f5e7ef
Amin Oruji <aminpy@gmail.com>
2012-07-02 12:26:57 +0430

    some bug fixed

--- jalali_convertor.py
+++ jalali_convertor.py
@@ -14,7 +14,20 @@ class JalaliConvertorWizard(osv.osv_memory):
     _defaults = {'state': 'init'}
 
     def do_convert(self, cr, uid, ids, context=None):
-        print "durud"
+        this = self.browse(cr, uid, ids, context=context)[0]
+#        ir_model_field_list = this.ir_model_id.field_id
+
+#        field_list = []
+#
+#        for ir_model_field in ir_model_field_list:
+#            if ir_model_field.name.endswith("_group_by"):
+#                field_list.append(ir_model_field.name[:-9])
+#
+#        if field_list:
+        input_model_obj = self.pool.get(this.ir_model_id.model)
+        input_id_list = input_model_obj.search(cr, uid, [], context=context)
+        input_model_obj.write(cr, uid, input_id_list, {}, context=context)
+
         return self.write(cr, uid, ids, {'state': 'done'}, context=context)
 
 JalaliConvertorWizard()
